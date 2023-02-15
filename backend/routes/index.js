456d6484b9c457479de771f54372b3c43e621fcd
Dongliang Li <dongliang14@gmail.com>
2023-02-15 13:12:32 -0800

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
@@ -9,12 +11,14 @@ const router = express.Router();
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
   });
+});
 
+
+router.use('/api', apiRouter);
 
 module.exports = router;
