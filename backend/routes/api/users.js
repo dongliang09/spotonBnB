af94b5e27375dcfd71f3df081a1c523c870fa868
Dongliang Li <dongliang14@gmail.com>
2023-02-24 13:48:43 -0800

    login update

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const bcrypt = require('bcryptjs');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -12,15 +13,20 @@ const validateSignup = [
     check('email')
       .exists({ checkFalsy: true })
       .isEmail()
-      .withMessage('Please provide a valid email.'),
+      .withMessage('Invalid email'),
     check('username')
       .exists({ checkFalsy: true })
-      .isLength({ min: 4 })
-      .withMessage('Please provide a username with at least 4 characters.'),
+      .withMessage('Username is required'),
     check('username')
       .not()
       .isEmail()
-      .withMessage('Username cannot be an email.'),
+      .withMessage('Username cannot be an email'),
+    check('firstName')
+      .exists({ checkFalsy: true })
+      .withMessage('First Name is required'),
+    check('lastName')
+      .exists({ checkFalsy: true })
+      .withMessage('Last Name is required'),
     check('password')
       .exists({ checkFalsy: true })
       .isLength({ min: 6 })
@@ -29,14 +35,55 @@ const validateSignup = [
 ];
 
 // Sign up
-router.post(
-    '/',
-    validateSignup,
-    async (req, res) => {
-      const { email, password, username, firstName, lastName } = req.body;
-      const user = await User.signup({ email, username, password, firstName, lastName });
+router.post('/',validateSignup,async (req, res) => {
 
-      await setTokenCookie(res, user);
+      const { email, password, username, firstName, lastName } = req.body;
+
+      //find if user/email exists
+      const usernameFound = await User.findOne({
+        where:{username: username}
+      })
+
+      if (usernameFound) {
+        return res.status(403).json({
+          "message": "User already exists",
+          "statusCode": 403,
+          "errors": {
+            "username": "User with that username already exists"
+          }
+        })
+      }
+
+      const emailFound = await User.findOne({
+        where:{email: email}
+      })
+
+      if (emailFound) {
+        return res.status(403).json({
+          "message": "User already exists",
+          "statusCode": 403,
+          "errors": {
+            "email": "User with that email already exists"
+          }
+        })
+      }
+      // const user = await User.signup({ email, username, password, firstName, lastName });
+
+      const hashedPassword = bcrypt.hashSync(password);
+
+      const newUser = await User.create({
+        username,
+        email,
+        hashedPassword,
+        firstName,
+        lastName
+      });
+
+      let newUserFound = await User.scope('currentUser').findByPk(newUser.id);
+      let token = await setTokenCookie(res, newUserFound);
+
+      user = newUserFound.toJSON();
+      user.token = token;
 
       return res.json({
         user: user
