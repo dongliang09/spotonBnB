8dcd136fcdd29ad8c8ea771e2995e238fd9bb491
Dongliang Li <dongliang14@gmail.com>
2024-06-26 13:36:54 -0700

    chai GET /spots/current initial

--- backend/test/testSuitesForRoutes/spotRoutes.js
+++ backend/test/testSuitesForRoutes/spotRoutes.js
@@ -9,21 +9,57 @@ const { expect } = chai;
 chai.should();
 chai.use(chaiHttp);
 
+/*
+POST    /api/session : login
+GET     /api/spots : get all spots on first page without authentication, but can see all
+GET     /api/spots/current : require authentication
+GET     /api/spots/:sptId
+POST    /api/spots
+POST    /api/spots/:spotId/images
+PUT     /api/spots/:sptId
+DELETE  /api/spots/:sptId
+GET     /api/spots/:spotId/reviews
+POST    /api/spots/:spotId/reviews
+GET     /api/spots/:spotId/bookings
+POST    /api/spots/:spotId/bookings
+*/
+
+/*
+postman set up cookies xsrftoken in the environment in route /api/csrf/restore
+pm.environment.set('xsrftoken', pm.cookies.get('XSRF-TOKEN'))
+
+example
+chai.request(url)
+  .get('/')
+  .set('Cookie', 'cookieName=cookieValue;otherName=otherValue')
+  .then(...)
+*/
+
+let xsrftoken = "";
+let csrftoken = "";
+let jwt = "";
+
 describe ('login', () => {
-  // postman set up cookies xsrftoken in the environment in route /api/csrf/restore
-  // pm.environment.set('xsrftoken', pm.cookies.get('XSRF-TOKEN'))
 
-  // example
-  // chai.request(url)
-  //   .get('/')
-  //   .set('Cookie', 'cookieName=cookieValue;otherName=otherValue')
-  //   .then(...)
-
-  let xsrftoken = "";
-  let csrftoken = "";
   chai.request(app).get('/api/csrf/restore').end((err, res) => {
     xsrftoken = res.body['XSRF-Token']
     csrftoken = res.header['set-cookie'][0].split(";")[0].split("=")[1];
+  })
+
+  it('login with wrong user information', (done) => {
+    chai.request(app).post('/api/session')
+      .set("XSRF-TOKEN", xsrftoken)
+      .set("Cookie", `_csrf=${csrftoken}`)
+      .set('content-type', 'application/json')
+      .send({
+        "credential":"Demo-lition123",
+        "password":"notPassword"
+      }).end((err, res) => {
+        res.should.have.status(401);
+        expect(res.body.title).to.equal("Login failed");
+        expect(res.body.message).to.equal("Invalid credentials");
+        done()
+    })
   })
 
   it('login with user information', (done) => {
@@ -42,16 +78,16 @@ describe ('login', () => {
         res.body['user'].should.have.property('email');
         res.body['user'].should.have.property('firstName');
         res.body['user'].should.have.property('lastName');
-        // console.log(res.body)
+        jwt = res.header['set-cookie'][0].split(";")[0].split("=")[1];
         done()
     })
   })
 
-
 })
 
 describe ('spot routes', () => {
-  it('get', (done) => {
+
+  it('get all spots', (done) => {
     chai.request(app).get('/api/spots').end((err, res) => {
       res.should.have.status(200);
       res.body.should.have.property('Spots');
@@ -67,31 +103,15 @@ describe ('spot routes', () => {
   // set up before each to seed some data??
 
 
-  // should return 403 forbidden when there is no authentication
-  // it('post with no authenticaion', (done) => {
-  //   chai.request(app).post('/api/spots')
-  //     .set('content-type', 'application/json')
-  //     .set("XSRF-TOKEN", xsrftoken)
-  //     .set("Cookie", _csrf=csrftoken)
-  //     .send({
-  //       address: "234 Disney Lane",
-  //       city: "San Francisco",
-  //       state: "California",
-  //       country: "United States of America",
-  //       lat: 37.7645358,
-  //       lng: -122.4730327,
-  //       name: "App Academy (Test2)",
-  //       description: "Place where web developers are created",
-  //       price: 234
-  //     })
-  //     .end((err, res) => {
-  //     console.log(res)
-  //       res.should.have.status(200);
-
-  //     // expect(res.body.message).to.equal("Forbidden");
-  //     done();
-  //   })
-  // })
+  // should return 401 require authentication
+  it("get current user's spots without authenticaion", (done) => {
+    chai.request(app).get('/api/spots/current')
+      .end((err, res) => {
+        res.should.have.status(401);
+        expect(res.body.message).to.equal("Authentication required");
+        done();
+    })
+  })
 
   // set up auth before each test?
   // beforeEach((done) => {
@@ -101,15 +121,35 @@ describe ('spot routes', () => {
   //   done()
   // })
 
-  // it('get current', (done) => {
-  //   let newSpot = {
-
-  //   }
-  //   // chai.request(app).get('/api/spots').end((err, response) => {
-  //   //   response.should.have.status(200);
-  //   // })
-  //   done();
-  // })
+  it("get current user's spots", (done) => {
+    chai.request(app).get('/api/spots/current')
+      .set("Cookie", `token=${jwt}`)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('Spots');
+        res.body['Spots'].should.be.a('array');
+        if (res.body['Spots'].length !== 0) {
+          let oneSpot = res.body['Spots'][0];
+          oneSpot.should.be.a('object');
+          oneSpot.should.have.property('id');
+          oneSpot.should.have.property('address');
+          oneSpot.should.have.property('city');
+          oneSpot.should.have.property('state');
+          oneSpot.should.have.property('country');
+          oneSpot.should.have.property('lat');
+          oneSpot.should.have.property('lng');
+          oneSpot.should.have.property('name');
+          oneSpot.should.have.property('description');
+          oneSpot.should.have.property('price');
+          oneSpot.should.have.property('ownerId');
+          oneSpot.should.have.property('createdAt');
+          oneSpot.should.have.property('updatedAt');
+          oneSpot.should.have.property('avgRating');
+          oneSpot.should.have.property('previewImage');
+        }
+        done();
+      })
+  })
 
 })
 
